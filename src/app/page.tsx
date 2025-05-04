d1bcd754040c66642a2a3762a37cbb5217356518
Chen Weizhi <d2d.weizhi@gmail.com>
2025-05-04 11:34:14 +0800

    Added the playbackRate for the dingAudio.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -263,6 +263,7 @@ export default function Home() {
 	useEffect(() => {
 		const storedSessId = localStorage.getItem("userSessId");
 		const dingAudio = new Audio("/ding.mp3");
+		dingAudio.playbackRate = 1.5;		// Set the default playbackRate to 1.5. Will tweak this with further testing.
 		
 		if (storedSessId) {
 			// continue with last session.
@@ -377,6 +378,7 @@ export default function Home() {
 				// When a break has ended, we will play the ding.mp3 file twice as a notification.
 				dingAudio.play();
 				dingAudio.onended = () => dingAudio.play();
+				dingAudio.onended = null;	// Tested, the "ding" sound won't stop. So we will stop it here.
 
 				setActivePeriodType('focus');
 				setCurrCycle(prevCycle => prevCycle + 1);
